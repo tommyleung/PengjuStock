9283037075c868b654f3d7a22c77aad5f7f3be00
nick <nick@nickdeMacBook-Pro.local>
2017-12-09 15:03:34 +0800

    update

--- python/tools/treeClassification.py
+++ python/tools/treeClassification.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import sklearn
+import sklearn.tree as tree
 import logRegres
+import matplotlib.pyplot as plt
 import numpy as np
 
 
 # sigmoid function
 def nonlin(x, deriv=False):
     if (deriv == True):
-        return x * (1 - x)
+        return np.exp(-x) * (1 + np.exp(-x))
     return 1 / (1 + np.exp(-x))
 
 
@@ -19,7 +22,7 @@ X = np.array([[0, 0, 1],
               [1, 1, 1]])
 
 # output dataset
-y = np.array([[0, 0, 1, 1]]).T
+y = np.array([[1, 1, 0, 1]]).T
 
 # seed random numbers to make calculation
 # deterministic (just a good practice)
@@ -37,12 +40,12 @@ print 'dot:', np.dot(X, syn0)
 print 'dot:', logRegres.sigmoid(np.dot(X, syn0))
 # quit()
 
-for iterator in xrange(10000):
+for iterator in xrange(100):
     # forward propagation
     l0 = X
 
     l1 = nonlin(np.dot(l0, syn0))
-    # print 'l1:', l1
+    print 'l1:', l1
 
     # how much did we miss?
     l1_error = y - l1
@@ -50,7 +53,9 @@ for iterator in xrange(10000):
     #print 'l1_error:', l1_error
 
     # multiply how much we missed by the
-    # slope of the sigmoid at the values in l1
+    # slope of the sigmssoid at the values in l1
+    # print 'nonlin:', nonlin(l1, True), 'l1:', l1
+
     l1_delta = l1_error * nonlin(l1, True)
     #print 'l1_delata:', l1_delta
 
