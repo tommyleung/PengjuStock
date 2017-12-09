9c21e8eca06199a92eb7127a34815668e1eb1929
nick <nick@nickdeMacBook-Pro.local>
2017-12-09 15:16:42 +0800

    update

---
+++ python/algorithm/dupangAnalysis.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# 杜邦分析法计算
--- python/tools/testBase.py
+++ python/tools/testBase.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 import numpy as np
-print 'numpy version:' + np.__version__ + '\n'
+import numpy.linalg as npl
 
+print 'numpy version:' + np.__version__ + '\n'
 
 # matrix basic calculation
 # matrix definition
@@ -18,9 +19,20 @@ y = np.array([[0, 1, 1]]).T;
 # matrix multiplicative
 z = x.dot(y);
 
-print x;
-print y;
-print z;
+# print 'print x:' + x;
+# print 'print y' + y;
+# print 'print z' + z;
+
+# numpy学习
+
+# 切片
+list = range(10)
+# print 'list all:', list;
+# print 'list[2:4]:', list[2:4]
+
+
+# numpy矩阵运算
+# 矩阵定义
 
 
 
