9a06025aad739d11de403867987848cc4851a28f
nick <nick@nickdeMacBook-Pro.local>
2017-12-11 19:24:57 +0800

    update; git push

--- python/tools/testBase.py
+++ python/tools/testBase.py
@@ -101,6 +101,9 @@ from pybrain.utilities import percentError
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure.modules import SoftmaxLayer
+from pylab import ion, ioff, figure, draw, contourf, clf, show, hold, plot
+from scipy import diag, arange, meshgrid, where
+from numpy.random import multivariate_normal
 
 # create network
 fnn = FeedForwardNetwork()
